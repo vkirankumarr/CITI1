d205c480cd6abb12ab4ebd16dcdaf51aece7eedf
vkirankumarr <vkirankumarr@gmail.com>
2019-01-31 10:36:58 +0530

    CITI PROJ
    CITI PROJ

---
+++ CIT-PJ-SANDBOX-L101/src/main/java/com/citi/CitPjSandboxL101Application.java
@@ -0,0 +1,11 @@
+package com.citi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CitPjSandboxL101Application {
+	public static void main(String[] args) {
+		SpringApplication.run(CitPjSandboxL101Application.class, args);
+	}
+}
+
